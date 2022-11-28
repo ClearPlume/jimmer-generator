3a339537402e5c69567c14e2868d431cb6d35919
fallenangel <the.fallenangel.965@gmail.com>
2022-11-28 17:16:43 +0800

    DummyVirtualFile#getPath调整   "/$fileName" -> filename

--- src/main/kotlin/top/fallenangel/jimmergenerator/model/dummy/DummyVirtualFile.kt
+++ src/main/kotlin/top/fallenangel/jimmergenerator/model/dummy/DummyVirtualFile.kt
@@ -5,5 +5,5 @@ import com.intellij.openapi.vfs.newvfs.impl.StubVirtualFile
 class DummyVirtualFile(private val fileName: String) : StubVirtualFile() {
     override fun getName() = fileName
 
-    override fun getPath() = "/$fileName"
+    override fun getPath() = fileName
 }
