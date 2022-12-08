f558b27a8df71cf776ce1aab13be00795e121193
fallenangel <the.fallenangel.965@gmail.com>
2022-12-08 16:12:05 +0800

    "表映射"表中   类型列调整，装填所有类型，设置类型时从传入值中拆分类型和包名

--- src/main/kotlin/top/fallenangel/jimmergenerator/ui/table/column/TypeColumnInfo.kt
+++ src/main/kotlin/top/fallenangel/jimmergenerator/ui/table/column/TypeColumnInfo.kt
@@ -3,6 +3,7 @@ package top.fallenangel.jimmergenerator.ui.table.column
 import com.intellij.openapi.ui.ComboBox
 import com.intellij.ui.components.JBTextField
 import com.intellij.util.ui.ColumnInfo
+import top.fallenangel.jimmergenerator.component.SettingStorageComponent
 import top.fallenangel.jimmergenerator.model.DbObj
 import top.fallenangel.jimmergenerator.model.type.Class
 import javax.swing.DefaultCellEditor
@@ -11,7 +12,11 @@ import javax.swing.table.TableCellRenderer
 
 class TypeColumnInfo(name: String) : ColumnInfo<DbObj, String>(name) {
     override fun setValue(item: DbObj, value: String) {
-        item.type = Class(value, item.type.`package`, item.type.nullable)
+        item.type = Class(
+            value.substringAfterLast('.'),
+            value.substringBeforeLast('.', ""),
+            item.type.nullable
+        )
     }
 
     override fun valueOf(item: DbObj) = item.type.name
@@ -25,7 +30,9 @@ class TypeColumnInfo(name: String) : ColumnInfo<DbObj, String>(name) {
     override fun isCellEditable(item: DbObj) = !item.isTable
 
     override fun getEditor(item: DbObj): TableCellEditor {
-        val editor = DefaultCellEditor(ComboBox(arrayOf("String", "Int", "Boolean")))
+        val typeMappings = SettingStorageComponent.storage.state.typeMappings
+        val types = (typeMappings.map { it.java } + typeMappings.map { it.javaPrimitives }).filterNot { it.isNullOrBlank() }.toTypedArray()
+        val editor = DefaultCellEditor(ComboBox(types))
         editor.clickCountToStart = 2
         return editor
     }
