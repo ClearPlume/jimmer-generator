5784141b936b7903556ef97cc17a0ac0e24989b3
fallenangel <the.fallenangel.965@gmail.com>
2022-12-09 09:45:21 +0800

    Reference新增次级构造器

--- src/main/kotlin/top/fallenangel/jimmergenerator/util/Reference.kt
+++ src/main/kotlin/top/fallenangel/jimmergenerator/util/Reference.kt
@@ -1,5 +1,9 @@
 package top.fallenangel.jimmergenerator.util
 
-class Reference<T : Any> {
+class Reference<T : Any>() {
     lateinit var value: T
+
+    constructor(value: T) : this() {
+        this.value = value
+    }
 }
