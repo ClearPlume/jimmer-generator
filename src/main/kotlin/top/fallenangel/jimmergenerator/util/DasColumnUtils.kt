07f4cedcc6b5e2f2da1563bcb56350af78e0ac36
the_FallenAngel <the.fallenangel.965@gmail.com>
2023-05-17 10:12:59 +0800

    fix: 去除表列名前后缀时，额外考虑下划线
    去除id
    id_user/user_id -> user

--- src/main/kotlin/top/fallenangel/jimmergenerator/util/DasColumnUtils.kt
+++ src/main/kotlin/top/fallenangel/jimmergenerator/util/DasColumnUtils.kt
@@ -18,8 +18,8 @@ import top.fallenangel.jimmergenerator.model.type.Parameter
  */
 fun DasNamed.field2property(prefix: String = "", suffix: String = "", uncapitalize: Boolean = false): String {
     var property = name.lowercase()
-    property = property.replaceFirst(Regex("^$prefix", RegexOption.IGNORE_CASE), "")
-    property = property.replaceFirst(Regex("$suffix$", RegexOption.IGNORE_CASE), "")
+    property = property.replaceFirst(Regex("^${prefix}_?", RegexOption.IGNORE_CASE), "")
+    property = property.replaceFirst(Regex("_?$suffix$", RegexOption.IGNORE_CASE), "")
     return property.sneak2camel(uncapitalize)
 }
 
